46b349ffcfa514dcb4e9c44c1ffcc653ed63babf
Jordi Tomas <Jordi00010@gmail.com>
2025-02-28 18:39:11 +0100

    Implement Header and cart

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MatToolbarModule, MatIconModule, MatBadgeModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  cartCount = signal(0);
 
+  constructor(private readonly cartService: CartService) {
+    this.cartService.getCartCount().subscribe(count => this.cartCount.set(count));
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  private readonly cartCount = signal(0);
 
-  constructor() { }
+  getCartCount(): Observable<number> {
+    return of(this.cartCount());
+  }
+
+  addToCart(): void {
+    this.cartCount.set(this.cartCount() + 1);
+  }
 }
