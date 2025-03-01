d94f40b93e90323b19fa50f0b155d8be03006e12
Jordi Tomas <Jordi00010@gmail.com>
2025-03-01 17:37:32 +0100

    Create product details layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
-import { Routes } from '@angular/router';
-import { provideRouter } from '@angular/router';
+import { Routes, provideRouter } from '@angular/router';
 import { ProductListComponent } from './features/product/product-list/product-list.component';
 import { ProductDetailsComponent } from './features/product/product-details/product-details.component';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
 
 export const appRouting = provideRouter(routes);
--- src/app/features/product/product-details/product-details.component.ts
+++ src/app/features/product/product-details/product-details.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../../services/product.service';
+import { ProductDTO } from '../../../models/product.dto';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
 })
 export class ProductDetailsComponent {
+  private readonly productService = inject(ProductService);
+  private readonly route = inject(ActivatedRoute);
 
+  product = signal<ProductDTO | null>(null);
+
+  constructor() {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.productService
+        .getProductById(productId)
+        .subscribe((product) => this.product.set(product));
+    }
+  }
 }
